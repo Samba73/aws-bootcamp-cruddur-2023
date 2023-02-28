2b89f7b3d735c438dbff460d14cc575fce2bc1ba
Samba73 <sambasivam.k@gmail.com>
2023-02-28 22:45:56 +0000

    changetolog

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -30,7 +30,7 @@ from time import strftime
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='Cruddur')
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
 LOGGER.addHandler(console_handler)
 LOGGER.addHandler(cw_handler)
 LOGGER.info('Watchtower:CW Log')
