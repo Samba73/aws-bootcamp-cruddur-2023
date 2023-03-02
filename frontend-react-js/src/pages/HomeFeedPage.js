a84a5ac7f747effe6e6b334d3d156c884ab7166c
Samba73 <sambasivam.k@gmail.com>
2023-03-02 23:30:21 +0000

    still not able to get frontend trace

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -7,6 +7,28 @@ import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
 
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { Resource }  from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const exporter = new OTLPTraceExporter({
+  url: "api.honeycomb.io:443",
+  headers: "x-honeycomb-team=${HONEYCOMB_API_KEY}"
+
+});
+const provider = new WebTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'react-js',
+  }),
+});
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+provider.register({
+  contextManager: new ZoneContextManager()
+});
+
+
 // [TODO] Authenication
 import Cookies from 'js-cookie'
 
--- frontend-react-js/src/tracing.js
+++ frontend-react-js/src/tracing.js
@@ -7,12 +7,13 @@ import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 
 
 const exporter = new OTLPTraceExporter({
-  url: 'https://api.honeycomb.io:443/v1/traces',
-  serviceName: "React-js"
+  url: "api.honeycomb.io:443",
+  headers: "x-honeycomb-team=${HONEYCOMB_API_KEY}"
+
 });
 const provider = new WebTracerProvider({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'browser',
+    [SemanticResourceAttributes.SERVICE_NAME]: 'react-js',
   }),
 });
 provider.addSpanProcessor(new BatchSpanProcessor(exporter));
