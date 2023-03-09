19d97553d8bd0d6189ebd1bf7e4582aacbdd1a0b
Samba73 <sambasivam.k@gmail.com>
2023-03-09 22:42:30 +0000

    updated the home_activities.py to render different result

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,9 +6,10 @@ tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run(cognito_user_id=None):
     #logger.info("HomeActivities")
+    with tracer.start_as_current_span("home-activites-mock-data"):
+      span = trace.get_current_span()
     if cognito_user_id != None:
-      with tracer.start_as_current_span("home-activites-mock-data"):
-        span = trace.get_current_span()
+
         now = datetime.now(timezone.utc).astimezone()
         span.set_attribute("app.now", now.isoformat())
 
@@ -51,18 +52,11 @@ class HomeActivities:
           'replies': []
         }
         ]
+    else:
 
-      if cognito_user_id != None:
-        extra_crud = {
-          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Lore',
-          'message': 'My dear brother, it the humans that are the problem',
-          'created_at': (now - timedelta(hours=1)).isoformat(),
-          'expires_at': (now + timedelta(hours=12)).isoformat(),
-          'likes': 1042,
-          'replies': []
-        }
-        results.insert(0,extra_crud)
+      results = [{
+        'message': 'User not authenticated'
+      }]
 
-      span.set_attribute("app.result_length", len(results))
-      return results
+    span.set_attribute("app.result_length", len(results))
+    return results
