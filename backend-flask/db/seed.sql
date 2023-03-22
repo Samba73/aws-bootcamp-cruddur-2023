0b6e594da94c803bbf94b2529405082761b539b3
Samba73 <sambasivam.k@gmail.com>
2023-03-22 04:26:51 +0000

    updated docker-compose for volumes section

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,4 +3,4 @@ INSERT INTO public.users(display_name, handle, cognito_user_id) VALUES
 
 INSERT INTO public.activities(user_uuid, message, expires_at) VALUES 
 ((select uuid from public.users where users.handle='samba' LIMIT 1),'Test message',
-current_timestamp + '10' );
+current_timestamp + interval '10 day' );
