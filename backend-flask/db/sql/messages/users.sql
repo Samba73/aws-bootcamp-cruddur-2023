d95a96a42547de18cd2be88bbcd109e092f0d0e8
Samba73 <sambasivam.k@gmail.com>
2023-03-25 10:02:04 +0000

    create_message is done

---
+++ backend-flask/db/sql/messages/users.sql
@@ -0,0 +1,12 @@
+  sql = """
+    SELECT 
+      users.uuid,
+      users.display_name,
+      users.handle
+    FROM users
+    WHERE
+      users.handle IN(
+        %(my_handle)s,
+        %(other_handle)s
+        )
+  """
