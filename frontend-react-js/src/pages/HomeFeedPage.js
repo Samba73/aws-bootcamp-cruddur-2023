fe8355f78da1dd7f425539d02d49a3cc848616a4
Samba73 <90577515+Samba73@users.noreply.github.com>
2023-04-08 11:44:11 +0530

    Update HomeFeedPage.js

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -19,7 +19,7 @@ export default function HomeFeedPage() {
 
   const loadData = async () => {
     try {
-      await getAuth
+      await getAuth()
       const access_token = localStorage.getItem("access_token")
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
