7d224109ffe80b7442ff72f274e4d9384292dd55
Samba73 <90577515+Samba73@users.noreply.github.com>
2023-04-08 11:30:24 +0530

    Updated for session persistence

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -6,7 +6,7 @@ import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
 import MessagesFeed from '../components/MessageFeed';
 import MessagesForm from '../components/MessageForm';
-import checkAuth from '../lib/CheckAuth';
+import { checkAuth, getAuth } from '../lib/CheckAuth';
 
 export default function MessageGroupPage() {
   const [messageGroups, setMessageGroups] = React.useState([]);
@@ -18,10 +18,12 @@ export default function MessageGroupPage() {
 
   const loadMessageGroupsData = async () => {
     try {
+      await getAuth
+      access_token = localStorage.getItem("access_token")
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
       const res = await fetch(backend_url, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+          Authorization: `Bearer ${access_token}`
         },
         method: "GET"
       });
