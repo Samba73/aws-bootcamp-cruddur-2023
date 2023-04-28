9e94fc96123260c28270be4f9b2f0205feded351
Samba73 <90577515+Samba73@users.noreply.github.com>
2023-04-28 17:13:50 +0530

    change folder for userprofile query

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -14,7 +14,7 @@ class UserActivities:
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
-      sql = extract_query('messages','userProfile')
+      sql = extract_query('users','userProfile')
       results = query_execution_select(sql,{'handle': user_handle})
       model['data'] = results
     return model
