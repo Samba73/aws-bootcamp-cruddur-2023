b41f5e74f9548efeb947c678c92bfcbaa127c211
Samba73 <90577515+Samba73@users.noreply.github.com>
2023-05-14 14:11:23 +0530

    added cognito_user_id in Avatar

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -1,5 +1,6 @@
 import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
+import ProfileAvatar from 'components/ProfileAvatar';
 
 export default function ProfileHeading(props) {
   console.log('props', props)
@@ -14,9 +15,7 @@ export default function ProfileHeading(props) {
     <div className='title'>{props.profile.display_name}</div>
     <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
     <div className="banner" style={styles} >
-      <div className="avatar">
-        <img src="https://assets.cruddur.in/avatars/data.jpg"></img>
-      </div>
+      <ProfileAvatar id={props.profile.cognito_user_uuid} />
     </div>
     <div className="info">
       <div className='id'>
