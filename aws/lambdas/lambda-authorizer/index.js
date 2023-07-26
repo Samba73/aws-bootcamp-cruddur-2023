85ec56329312ee09162094606224ece425ab0bfe
Samba73 <sambasivam.k@gmail.com>
2023-07-26 07:56:36 +0000

    replaced lambdaauthorizer code with latest

--- aws/lambdas/lambda-authorizer/index.js
+++ aws/lambdas/lambda-authorizer/index.js
@@ -4,15 +4,17 @@ const { CognitoJwtVerifier } = require("aws-jwt-verify");
 const jwtVerifier = CognitoJwtVerifier.create({
   userPoolId: process.env.USER_POOL_ID,
   tokenUse: "access",
-  clientId: process.env.CLIENT_ID
+  clientId: process.env.CLIENT_ID,
+
 });
 
 exports.handler = async (event) => {
   console.log("request:", JSON.stringify(event, undefined, 2));
+  const token = JSON.stringify(event.headers["authorization"]).split(" ")[1].replace(/['"]+/g, '');
 
-  const jwt = event.headers.authorization;
   try {
-    const payload = await jwtVerifier.verify(jwt);
+    const payload = await jwtVerifier.verify(token);
+    console.log('payload', payload);
     console.log("Access allowed. JWT payload:", payload);
   } catch (err) {
     console.error("Access forbidden:", err);
@@ -24,3 +26,4 @@ exports.handler = async (event) => {
     isAuthorized: true,
   };
 };
+
